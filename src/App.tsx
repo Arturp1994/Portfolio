3c5ad958e6f90affa124392ade14d284e2355aad
Arthur <arturiopr1994@gmail.com>
2022-11-06 21:27:21 -0800

    Portfolio v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Remote from "./remote/Remote";
 import Contacts from "./contacts/Contacts";
 import Footer from "./footer/Footer";
+import Navbar from "./nav/Nav";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            <Navbar/>
+
+
             <Main/>
             <Skills/>
             <Projects/>
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -1,15 +1,59 @@
 import React from 'react';
-import style from './Nav.module.css'
+import style from './Nav.module.scss'
+import image from './../assets/image/photo.jpeg'
 
-function Nav() {
+function Navbar() {
     return (
-        <div className={style.nav}>
-            <a href=''>Главная</a>
-            <a href=''>Скиллы</a>
-            <a href=''>Проекты</a>
-            <a href=''>Контакты</a>
-        </div>
+        <nav className={style.nav}>
+            <div className={style.container} style={{backgroundImage: `url(${image})`}}>
+                {/*Button Menu*/}
+                {/* List link*/}
+                <div className={style.navbar_collapse}>
+                    <div className={style.logo}></div>
+                    <ul className={style.navbar_nav}>
+                        <li className={style.nav_item}>
+                            <a className={style.nav_link} href=''>
+                                HOME
+                            </a>
+                        </li>
+                        <li className={style.nav_item}>
+                            <a className={style.nav_link} href=''>
+                                ABOUT
+                            </a>
+                        </li>
+                        <li className={style.nav_item}>
+                            <a className={style.nav_link} href=''>
+                                PORTFOLIO
+                            </a>
+                        </li>
+                        <li className={style.nav_item}>
+                            <a className={style.nav_link} href=''>
+                                CONTACT
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className={style.social_media_nav}>
+                        <li>
+                            <a className={style.facebook} href=''>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a className={style.lincedIn} href=''>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a className={style.gitHub} href=''>
+
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
-export default Nav;
+export default Navbar;
